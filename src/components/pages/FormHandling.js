f01416269626d1db6b0095d117d74bf0e3587c79
Jarodski <knightjarod7@gmail.com>
2020-11-30 00:00:15 +0800

    add to a list when submitted

--- src/components/pages/FormHandling.js
+++ src/components/pages/FormHandling.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react'
 
+let id = 0
+
 class FormHandling extends Component {
     state = {
-        username: "",
-        password: ""
+        note: "",
+        color: "red",
+        notes: []
 
     }
 
-    onChangeUsername = (e) => {
-        this.setState({ username: e.target.value })
+    onChangeNote = (e) => {
+        this.setState({
+            note: e.target.value,
+            color: this.state.color,
+            notes: this.state.notes
+        })
     }
+
+    onChangeColor = (e) => {
+        this.setState({
+            note: this.state.note,
+            color: e.target.value,
+            notes: this.state.notes
+        })
+    }
+
+    onSubmit = (e) => {
+        const { note, color, notes } = this.state
+        e.preventDefault()
+        this.setState({ note: "", color: "red", notes: [...notes, { note: note, color: color }] })
+    }
+
     render() {
+        const { note, color } = this.state
         return (
             <div>
-                <form>
-                    <label>Username: </label>
-                    <input type="text" name="username" id="username" value={this.state.username} onChange={this.onChangeUsername} />
+                <form onSubmit={this.onSubmit}>
+                    <label>Note: </label>
+                    <input type="text" name="note" id="note" value={note} onChange={this.onChangeNote} />
+                    <label>Color: </label>
+                    <select value={color} onChange={this.onChangeColor}>
+                        <option value="red">Red</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                    </select>
                     <input type="submit" value="Submit" />
+
+                    {this.state.notes.map(note => (
+                        <div style={{ border: "solid", borderWidth: 2, borderRadius: 4, padding: 2, margin: 2 }}>
+                            {note.note}, {note.color}
+                        </div>
+                    ))}
+
                 </form>
             </div>
         )
